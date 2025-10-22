8b7e878529ae2fe02cab13bca8fd456f2e8dc6ff
nikhil153 <nikhil153@gmail.com>
2025-10-22 18:34:29 -0400

    fixed roi_stats

--- extract_fwDTI_dipy_stats.py
+++ extract_fwDTI_dipy_stats.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 import json
 import nibabel as nib
+import logging
 
 from dipy.align import affine_registration
 from dipy.align import affine_registration, syn_registration, write_mapping, read_mapping
@@ -15,6 +16,13 @@ from dipy.align.imaffine import AffineMap
 from dipy.io.image import load_nifti
 from dipy.viz import regtools
 
+
+# configure logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 def align_and_resample(mov, ref, label=None, affine_config=None, syn_config=None, sub2ref_affine=None, sub2ref_mapping=None, warp=True):
     """
@@ -53,10 +61,10 @@ def align_and_resample(mov, ref, label=None, affine_config=None, syn_config=None
     """
 
     if sub2ref_affine is not None:
-        print(" --  --  -- Using provided affine for registration.")
+        logger.info(" --  --  -- Using provided affine for registration.")
 
     else:
-        print(" --  --  -- Computing affine registration.")
+        logger.info(" --  --  -- Computing affine registration.")
         _, sub2ref_affine = affine_registration(
             mov,
             ref,
@@ -85,9 +93,9 @@ def align_and_resample(mov, ref, label=None, affine_config=None, syn_config=None
 
     # compute non-linear warp if specified
     if warp:
-        print(" --  --  -- Computing non-linear warp registration.")
+        logger.info(" --  --  -- Computing non-linear warp registration.")
         if sub2ref_mapping is not None:
-            print(" --  --  -- Using provided warp for registration.")
+            logger.info(" --  --  -- Using provided warp for registration.")
 
         else:
             # still using the orginal moving image but with affine pre-alignment
@@ -135,10 +143,10 @@ def loadLabels(f_label_vol, f_label_map):
     # get the unique labels
     labels_in_vol = np.unique(label_vol.get_fdata())[1:].astype(int)  # skip the background label
 
-    print(f" -- Found {len(labels_in_vol)} labels in the volume: {f_label_vol}")
-
+    logger.info(f" -- Found {len(labels_in_vol)} labels in the volume: {f_label_vol}")
+ 
     # read label map file
-    print(f" -- Loading label map file: {f_label_map}")
+    logger.info(f" -- Loading label map file: {f_label_map}")
     label_map_df = pd.read_csv(f_label_map, sep="\t")
     
     # check map file columns
@@ -151,13 +159,13 @@ def loadLabels(f_label_vol, f_label_map):
     labels_in_map = label_map_df["label"].to_list()
     
     # print the text labels
-    print(f" -- Found {len(labels_in_map)} labels in the text file: {f_label_map}")
-    
+    logger.info(f" -- Found {len(labels_in_map)} labels in the text file: {f_label_map}")
+     
     # check if all the labels in the volume are in the text file
     if set(labels_in_vol).issubset(set(labels_in_map)):
-        print(" -- All volume labels are present in the text label file.")
+        logger.info(" -- All volume labels are present in the text label file.")
     else:
-        print(" -- Some volume labels are not present in the text label file.") 
+        logger.error(" -- Some volume labels are not present in the text label file.")
         raise ValueError("Not all volume labels are present in the text label file.")
 
     # pull the names from the text file
@@ -171,6 +179,7 @@ def loadLabels(f_label_vol, f_label_map):
 
 def load_nii(nii_path, loader="nib"):
     """ Load a nifti file either using nibabel or dipy """
+
     try:
         if loader == "dipy":
             vol, affine = load_nifti(nii_path)
@@ -180,8 +189,8 @@ def load_nii(nii_path, loader="nib"):
             affine = nii.affine
     
     except Exception as e:
-        print(f" -- Error loading nifti file: {nii_path}")
-        print(f" -- {e}")
+        logger.error(f" -- Error loading nifti file: {nii_path}")
+        logger.exception(e)
         vol = None
         affine = None
 
@@ -190,6 +199,7 @@ def load_nii(nii_path, loader="nib"):
 
 def getROIStats(data, label_mask, roi_label):
     """ Get statistics for a given ROI """
+
     roi_stats = {}
     roi_data = data[label_mask == roi_label]
 
@@ -210,7 +220,7 @@ parser.add_argument("-t", "--test_reg_images", help="save test registration imag
 
 args = parser.parse_args()
 
-dataset = args.dataset ## dataset = "/home/nikhil/projects/Parkinsons/nimhans/data/ylo/"
+dataset = args.dataset 
 participant_id = args.participant_id
 session_id = args.session_id
 config_file = args.config_file
@@ -229,16 +239,16 @@ if session_id.startswith("ses-"):
 session = f"ses-{session_id}"
 
 # read the config file
-print(f"Loading configuration file: {config_file}")
+logger.info(f"Loading configuration file: {config_file}")
 with open(config_file, 'r') as cf:
     config = json.load(cf)
-
-# preproc config
+ 
+ # preproc config
 preproc_pipeline_name = config.get("preproc_pipeline").get("name")
 preproc_pipeline_version = config.get("preproc_pipeline").get("version")
 preproc_pipeline_software = config.get("preproc_pipeline").get("software")
 
-print(f"Using preproc pipeline: {preproc_pipeline_name} - version: {preproc_pipeline_version}")
+logger.info(f"Using preproc pipeline: {preproc_pipeline_name} - version: {preproc_pipeline_version}")
 
 # reference space config
 ref_img = config.get("reference_space").get("image_path")
@@ -259,12 +269,12 @@ pipeline_idps_dir = f"{dataset}/derivatives/{preproc_pipeline_name}/{preproc_pip
 if save_test_reg_images:    
     test_reg_images_dir = "./test_reg_images"
     Path(test_reg_images_dir).mkdir(parents=True, exist_ok=True)
-    print(f"Saving registration QC images for testing at {test_reg_images_dir} for sanity checks.")
-
-print("Running extraction of estimated features.")
+    logger.info(f"Saving registration QC images for testing at {test_reg_images_dir} for sanity checks.")
+ 
+logger.info("Running extraction of estimated features.")
 
 # load label volume
-print("Loading and verifying label volume.")
+logger.info("Loading and verifying label volume.")
 
 # check and load the label image
 labs, labels, roi_labels = loadLabels(ref_label, label_map)
@@ -285,34 +295,34 @@ else:
 
 warp = not affine_only
 
-print(f"Using registration method: {reg_method} --> warp={warp}")
+logger.info(f"Using registration method: {reg_method} --> warp={warp}")
 
 # build input / output paths
 datadir = pipeline_output_dir
 outsdir = pipeline_idps_dir
 
 # for every subject
-print(f" -- Extracting IDP data from: {preproc_pipeline_name}-{preproc_pipeline_version}")
+logger.info(f" -- Extracting IDP data from: {preproc_pipeline_name}-{preproc_pipeline_version}")
 
-print(f" --  -- Processing: {bids_participant}")
+logger.info(f" --  -- Processing: {bids_participant}")
 
 # create output file name
 outfile = Path(outsdir, f"{bids_participant}_{session}_{preproc_pipeline_name}-{preproc_pipeline_version}_{parc}_{reg_method}_idp.tsv")
 
 # load the files to extract
-print(f" --  -- Extracting data from: {bids_participant}")
+logger.info(f" --  -- Extracting data from: {bids_participant}")
 dpdir = Path(datadir, bids_participant, f"{session}", "dipy")
 
 mov = nib.load(Path(dpdir, f"{bids_participant}_{session}_model-dti_param-fa_map.nii.gz"))
 
+# save original images for sanity checks
 if save_test_reg_images:
     nib.save(mov, f'{test_reg_images_dir}/sub_orig.nii.gz')
     nib.save(ref, f'{test_reg_images_dir}/ref_orig.nii.gz')
     nib.save(labs, f'{test_reg_images_dir}/label_orig.nii.gz')
-
-# linearly align dipy DTI FA (subject space) to the reference FA (JHU FA)
     
-print(" --  --  -- Starting alignment and resampling.")  
+logger.info(" --  --  -- Starting alignment and resampling.")  
+
 # path to subject affine file
 sub_aff_dir = f"{pipeline_output_dir}/{bids_participant}/{session}/affine/"
 subj_aff_stem = f"{bids_participant}_{session}_{atlas_name}_sub2ref.txt"
@@ -321,7 +331,7 @@ subj_aff_path = Path(sub_aff_dir, subj_aff_stem)
 # if the affine file exists, load it
 sub2ref_affine = None
 if subj_aff_path.exists():
-    print(f" --  --  -- Using existing affine: {subj_aff_stem}")
+    logger.info(f" --  --  -- Using existing affine: {subj_aff_stem}")
     sub2ref_affine = np.loadtxt(subj_aff_path)
 
 # path to subject warp file
@@ -332,10 +342,13 @@ subj_warp_path = Path(sub_warp_dir, subj_warp_stem)
 # load existing warp if exists
 sub2ref_mapping = None
 if warp & (subj_warp_path.exists()):
-    print(f"Using existing warp from: {subj_warp_path}")        
-    sub2ref_mapping = read_mapping(str(subj_warp_path), mov, ref)
+    logger.info(f"Using existing warp from: {subj_warp_path}")        
+    # Need to specify prealign as affine since it was used during syn computation
+    # For some reason it requires the inverse affine while reading the mapping
+    # see: https://github.com/dipy/dipy/discussions/3272
+    sub2ref_mapping = read_mapping(str(subj_warp_path), mov, ref, prealign=np.linalg.inv(sub2ref_affine))
 
-# # align and resample the moving image to the reference
+# align and resample the moving image to the reference
 sub_affined, sub_warped, label_inverse_affined, label_inverse_warped, sub2ref_affine, sub2ref_mapping = align_and_resample(
     mov, 
     ref, 
@@ -347,17 +360,18 @@ sub_affined, sub_warped, label_inverse_affined, label_inverse_warped, sub2ref_af
     warp=warp
     )
 
-print(" --  --  -- Alignment and resampling complete.")
+logger.info(" --  --  -- Alignment and resampling complete.")
+
 # save the affine to disk
 if not subj_aff_path.exists():
     Path(sub_aff_dir).mkdir(parents=True, exist_ok=True)
-    print(f" --  --  -- Saving affine to: {subj_aff_path}")
+    logger.info(f" --  --  -- Saving affine to: {subj_aff_path}")
     np.savetxt(subj_aff_path, sub2ref_affine)
     
 # save warps
 if warp & (not subj_warp_path.exists()):
     Path(sub_warp_dir).mkdir(parents=True, exist_ok=True)
-    print(f"Saving warp to: {subj_warp_path}")
+    logger.info(f"Saving warp to: {subj_warp_path}")
     write_mapping(sub2ref_mapping, subj_warp_path)
 
 # save the transformed images
@@ -375,12 +389,12 @@ else:
     tldat = label_inverse_affined.get_fdata().astype(int)
 
 # load and prep data for extraction
-# diff_params = ["fa","md","ad","nrmse","residual"]
+# diff_params = ["fa","md"]
 diff_params = diffusion_metrics.keys()
-print(f" --  --  -- Extracting diffusion metrics: {diff_params}")
-
+logger.info(f" --  --  -- Extracting diffusion metrics: {diff_params}")
+ 
 # load data files
-print(" --  --  -- Loading data files for extraction.")   
+logger.info(" --  --  -- Loading data files for extraction.")   
 summary_df = pd.DataFrame() 
 for diff_p in diff_params:
     diff_param_map, _ = load_nii(Path(dpdir, f"{bids_participant}_{session}_model-dti_param-{diff_p}_map.nii.gz"), loader="dipy")
@@ -391,10 +405,12 @@ for diff_p in diff_params:
     fwdti_roi_df = pd.DataFrame(columns=["roi_idx","model","param","mean_value","std_value","voxel_count"])
     for idx, roi in enumerate(labels):
         # Default
-        dti_val_mean, dti_val_std, dti_val_count = getROIStats(diff_param_map, tldat, roi)
+        roi_stats = getROIStats(diff_param_map, tldat, roi)
+        dti_val_mean, dti_val_std, dti_val_count = roi_stats["mean"], roi_stats["std"], roi_stats["count"]
         dti_roi_df.loc[idx] = [roi, "dti", diff_p, dti_val_mean, dti_val_std, dti_val_count]
         # FW
-        FW_val_mean, FW_val_std, FW_val_count = getROIStats(diff_FW_param_map, tldat, roi)
+        roi_stats = getROIStats(diff_FW_param_map, tldat, roi)
+        FW_val_mean, FW_val_std, FW_val_count = roi_stats["mean"], roi_stats["std"], roi_stats["count"]
         fwdti_roi_df.loc[idx] = [roi, "fwdti", diff_p, FW_val_mean, FW_val_std, FW_val_count]
 
     roi_df = pd.concat([dti_roi_df,fwdti_roi_df], axis=0)
@@ -408,6 +424,6 @@ summary_df = pd.merge(summary_df,label_map_df,on="roi_idx",how="left")
 
 # write the dataframe to disk
 summary_df.to_csv(outfile, index=False, header=True, sep="\t")
-print(f" --  -- Saved {bids_participant}_{session} IDPs to {outfile}.")
+logger.info(f" --  -- Saved {bids_participant}_{session} IDPs to {outfile}.")
 
-print("Done.")
+logger.info("IDP extraction complete!!")
